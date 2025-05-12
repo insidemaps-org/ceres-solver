4c61421c1b27d90996d7043ab3ce857e9e4e7f43
dejan <IMDejanjevtic@Dejans-MacBook-Pro.local>
2025-05-12 16:25:14 +0200

    Update package file.

--- Package.swift
+++ Package.swift
@@ -1,22 +1,20 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
-    name: "ceres",
-    platforms: [
-        .macOS(.v11), .iOS(.v12), .macCatalyst(.v16)
-    ],
-    products: [
-        .library(
-            name: "ceres",
-            targets: ["ceres"]
-        ),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "ceres",
-            url: "https://github.com/insidemaps-org/ceres-solver/releases/download/ceres-pod-2.1.v7.link.lib.with.xcode/ceres.xcframework.zip",
-            checksum: "6a194e7373c02cbfe6a2c91887a0a18f4117b668f972519f9dd66413554fe156"
-        ),
-    ]
+	name: "ceres",
+	platforms: [
+		.macOS(.v11), .iOS(.v12), .macCatalyst(.v16)
+	],
+	products: [
+		.library(name: "ceres" , targets: ["ceres"])
+	],
+	targets: [
+		.binaryTarget(
+			name: "ceres",
+			url: "https://github.com/insidemaps-org/ceres-solver/releases/download/ceres-pod-2.1.v7.link.lib.with.xcode/ceres.xcframework.zip",
+			checksum: "6a194e7373c02cbfe6a2c91887a0a18f4117b668f972519f9dd66413554fe156"
+		),
+	]
 )
+
